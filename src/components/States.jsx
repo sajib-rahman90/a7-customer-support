f28861b9a637a6d0ada5ad05056a84d4229d1b66
sajib-rahman90 <sajibrahman0090@gmail.com>
2025-12-20 12:03:15 +0600

    solved mobiel devices responsive problems of States component

--- src/components/States.jsx
+++ src/components/States.jsx
@@ -6,9 +6,9 @@ const States = ({ ticketTotal, resolvedTotal }) => {
       {/* card1 */}
 
       <div className="relative bg-[linear-gradient(125.07deg,rgba(99,46,227,1),rgba(159,98,242,1))] w-1/2 px-10 py-14 text-center rounded-lg overflow-hidden">
-        <div className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-contain bg-[url('/vector1.png')]"></div>
+        <div className="absolute md:top-0 left-0 w-full h-full bg-no-repeat bg-contain bg-[url('/vector1.png')]"></div>
 
-        <div className="absolute bottom-0 right-0  w-full h-full transform scale-x-[-1]  bg-no-repeat bg-contain bg-[url('/vector2.png')]"></div>
+        <div className="absolute md:bottom-0 right-0  w-full h-full transform scale-x-[-1]  bg-no-repeat bg-contain bg-[url('/vector2.png')]"></div>
 
         <div className="relative z-10">
           <p className="text-white font-normal text-[24px]">In-Progress</p>
@@ -18,9 +18,9 @@ const States = ({ ticketTotal, resolvedTotal }) => {
 
       {/* card2 */}
       <div className="relative overflow-hidden bg-[linear-gradient(90deg,rgba(84,207,103.68,1),rgba(0,130,122.2,1))] w-1/2 px-10 py-14 text-center rounded-lg">
-        <div className="absolute top-0 left-0 w-full h-full bg-no-repeat bg-contain bg-[url('/vector1.png')]"></div>
+        <div className="absolute md:top-0 left-0 w-full h-full bg-no-repeat bg-contain bg-[url('/vector1.png')]"></div>
 
-        <div className="absolute bottom-0 right-0  w-full h-full transform scale-x-[-1]  bg-no-repeat bg-contain bg-[url('/vector2.png')]"></div>
+        <div className="absolute md:bottom-0  right-0  w-full h-full transform scale-x-[-1]  bg-no-repeat bg-contain bg-[url('/vector2.png')]"></div>
         <div>
           <p className="text-white font-normal text-[24px]">Resolved</p>
           <h3 className="text-white font-semibold text-6xl">{resolvedTotal}</h3>
